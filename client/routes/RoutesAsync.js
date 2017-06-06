09fba895d279b5aff4810ce29fbce13d25809339
frv100ss <vincentgavignet@yahoo.fr>
2017-06-06 19:07:35 +0200

    Deactivating  loading msg

--- client/routes/RoutesAsync.js
+++ client/routes/RoutesAsync.js
@@ -2,52 +2,50 @@ import React from "react";
 // getComponent is a function that returns a promise for a component
 // It will not be called until the first mount
 export function asyncComponent(getComponent) {
-  return class AsyncComponent extends React.Component {
+    return class AsyncComponent extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.Component = null;
-      this.state = {Component: AsyncComponent.Component};
-    }
+        constructor(props) {
+            super(props);
+            this.Component = null;
+            this.state = {Component: AsyncComponent.Component};
+        }
 
-    componentWillMount() {
-      if (!this.state.Component) {
-        getComponent().then(Component => {
-          AsyncComponent.Component = Component;
-          this.setState({Component})
-        })
-      }
-    }
+        componentWillMount() {
+            if (!this.state.Component) {
+                getComponent().then(Component => {
+                    AsyncComponent.Component = Component;
+                    this.setState({Component})
+                })
+            }
+        }
 
-    render() {
-      const {Component} = this.state;
-      if (Component) {
-        return <Component {...this.props} />
-      }
-      return <LoadingMsg />
+        render() {
+            const {Component} = this.state;
+            if (Component) {
+                return <Component {...this.props} />
+            }
+            return null
+        }
     }
-  }
 }
 
-const LoadingMsg = () => {
-  return (
-    <div id='loadMsg'>
-      waiting Message
-    </div>
-  )
-};
+// const LoadingMsg = () => {
+//     return (
+//         <div id='loadMsg'>waiting Message</div>
+//     )
+// };
 
 
 export const HomePage = asyncComponent(() =>
-  System.import('../components/HomePage').then(module => module.default)
+    System.import('../components/HomePage').then(module => module.default)
 );
 
 export const LoginPage = asyncComponent(() =>
-  System.import('../containers/LoginPage').then(module => module.default)
+    System.import('../containers/LoginPage').then(module => module.default)
 );
 
 export const SignUpPage = asyncComponent(() =>
-  System.import('../containers/SignUpPage').then(module => module.default)
+    System.import('../containers/SignUpPage').then(module => module.default)
 );
 
 
